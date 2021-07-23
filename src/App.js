a0163f69173bc997ee387f01bd62e24f8defa950
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-23 15:22:45 -0300

    Class 353 - Using ReactTransitionGroup

--- src/App.js
+++ src/App.js
@@ -4,23 +4,45 @@ import './App.css';
 import Modal from './components/Modal/Modal';
 import Backdrop from './components/Backdrop/Backdrop';
 import List from './components/List/List';
+import { Transition } from 'react-transition-group';
 
 const App = () => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [showBlock, setShowBlock] = useState(false);
 
-  const showModal = () => setModalIsOpen(true);
-  const closeModal = () => setModalIsOpen(false);
+    const showModal = () => setModalIsOpen(true);
+    const closeModal = () => setModalIsOpen(false);
 
-  return (
-    <div className="App">
-      <h1>React Animations</h1>
-      <button className="Button" onClick={showModal}>Open Modal</button>
-      <h3>Animating Lists</h3>
-      {modalIsOpen && <Modal closed={closeModal} show={modalIsOpen} />}
-      {modalIsOpen && <Backdrop show={modalIsOpen} />}
-      <List />
-    </div>
-  );
-};
+    return (
+      <div className="App">
+        <h1>React Animations</h1>
+
+        <button className="Button" onClick={() => setShowBlock(!showBlock)}>Toggle</button>
+        <Transition
+          in={showBlock}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => <div style={{
+            backgroundColor: 'red',
+            width: 100,
+            height: 100,
+            margin: 'auto',
+            transition: 'opacity 1s ease-out',
+            opacity: state === 'exiting' ? 0 : 1
+          }} />}
+        </Transition>
+
+        <br />
+        <button className="Button" onClick={showModal}>Open Modal</button>
+        <h3>Animating Lists</h3>
+        {modalIsOpen && <Modal closed={closeModal} show={modalIsOpen} />}
+        {modalIsOpen && <Backdrop show={modalIsOpen} />}
+        <List />
+      </div>
+    );
+  }
+;
 
 export default App;
