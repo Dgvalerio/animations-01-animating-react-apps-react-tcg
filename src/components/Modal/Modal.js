0fa5702c507f1ab996a9ed028e793da55e437a93
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-24 17:28:05 -0300

    Class 358 - The CSSTransition Component

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Transition } from 'react-transition-group';
+import { CSSTransition } from 'react-transition-group';
 import './Modal.css';
 
 const animationTiming = {
@@ -8,27 +8,20 @@ const animationTiming = {
 };
 
 const modal = ({ show, closed }) => (
-  <Transition in={show} timeout={animationTiming} mountOnEnter unmountOnExit>
-    {(state) => {
-      const cssClasses = [
-        'Modal',
-        state === 'entering'
-          ? 'ModalOpen'
-          : state === 'exiting'
-          ? 'ModalClosed'
-          : null,
-      ];
-
-      return (
-        <div className={cssClasses.join(' ')}>
-          <h1>A Modal</h1>
-          <button type="button" className="Button" onClick={closed}>
-            Dismiss
-          </button>
-        </div>
-      );
-    }}
-  </Transition>
+  <CSSTransition
+    in={show}
+    timeout={animationTiming}
+    mountOnEnter
+    unmountOnExit
+    classNames="fade-slide"
+  >
+    <div className="Modal">
+      <h1>A Modal</h1>
+      <button type="button" className="Button" onClick={closed}>
+        Dismiss
+      </button>
+    </div>
+  </CSSTransition>
 );
 
 export default modal;
