67a9e72b049f078f18adccf6ceea91b0c8a200b8
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-24 17:21:44 -0300

    Class 357 - Transition Events

--- src/App.js
+++ src/App.js
@@ -24,7 +24,18 @@ const App = () => {
       >
         Toggle
       </button>
-      <Transition in={showBlock} timeout={300} mountOnEnter unmountOnExit>
+      <Transition
+        in={showBlock}
+        timeout={300}
+        mountOnEnter
+        unmountOnExit
+        onEnter={() => console.log('onEnter')}
+        onEntering={() => console.log('onEntering')}
+        onEntered={() => console.log('onEntered')}
+        onExit={() => console.log('onExit')}
+        onExiting={() => console.log('onExiting')}
+        onExited={() => console.log('onExited')}
+      >
         {(state) => (
           <div
             style={{
