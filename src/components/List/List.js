38f979678bff972ac8f13d31bdbf5b7cc98f6a9e
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-24 17:36:57 -0300

    Class 360 - Animating Lists

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,27 +1,25 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions,react/no-array-index-key */
 import React, { useState } from 'react';
-
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import './List.css';
 
 const list = () => {
   const [items, setItems] = useState([1, 2, 3]);
 
   const addItemHandler = () =>
-    setItems((prevState) => prevState.items.concat(prevState.items.length + 1));
+    setItems((prevState) => prevState.concat(prevState.length + 1));
 
   const removeItemHandler = (selIndex) =>
     setItems((prevState) =>
-      prevState.items.filter((item, index) => index !== selIndex)
+      prevState.filter((item, index) => index !== selIndex)
     );
 
   const listItems = items.map((item, index) => (
-    <li
-      key={index}
-      className="ListItem"
-      onClick={() => removeItemHandler(index)}
-    >
-      {item}
-    </li>
+    <CSSTransition key={index} classNames="fade" timeout={300}>
+      <li className="ListItem" onClick={() => removeItemHandler(index)}>
+        {item}
+      </li>
+    </CSSTransition>
   ));
 
   return (
@@ -30,7 +28,9 @@ const list = () => {
         Add Item
       </button>
       <p>Click Item to Remove.</p>
-      <ul className="List">{listItems}</ul>
+      <TransitionGroup component="ul" className="List">
+        {listItems}
+      </TransitionGroup>
     </div>
   );
 };
